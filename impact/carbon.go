b2e46451b2432d7b4b63fc8b28e9605200d83976
karim-daw <karimdaw91@gmail.com>
2024-04-04 22:59:54 +0100

    Interface: CarbonCalculator
    In essence, any object should be able to calculate carbon, as all objects have carbon, so this made sense to me

---
+++ impact/carbon.go
@@ -0,0 +1,15 @@
+package impact
+
+// CarbonCalculator defines the interface for calculating carbon impact
+type CarbonCalculator interface {
+
+	// ComputeWholeLifeCarbon calculates the total carbon impact of a material, assembly, or building
+	ComputeWholeLifeCarbon() float64
+
+	// CalculateCarbonForPhase calculates the carbon impact of a material, assembly, or building for a specific phase
+	CalculateCarbonForPhase(phase ...string) float64
+}
+
+type PhaseBasedCarbonCalculator interface {
+	CalculateCarbonForPhase(phase ...string) float64
+}
