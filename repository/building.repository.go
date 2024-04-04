07c84e51aff548c0db3497f26cd4721ea052faa6
karim-daw <karimdaw91@gmail.com>
2024-04-04 02:52:43 +0100

    Refactor: clean up and better practttise

--- repository/building.repository.go
+++ repository/building.repository.go
@@ -7,9 +7,9 @@ import (
 )
 
 type BuildingRepository interface {
-	Save(user *model.Building)
-	ExistsByBuildingName(username string) bool
-	FindByID(id int) (*model.Building, error)
+	Save(building *model.Building) error
+	ExistsByBuildingName(buildingName string) bool
+	FindByID(id uint) (*model.Building, error)
 	FindAll() ([]model.Building, error)
 }
 
@@ -17,43 +17,34 @@ type buildingRepository struct {
 	db *gorm.DB
 }
 
-func (repository *buildingRepository) Save(user *model.Building) {
-	repository.db.Save(user)
+func (r *buildingRepository) Save(building *model.Building) error {
+	return r.db.Save(building).Error
 }
 
-// ExistsByUsername checks if a user with the given username exists in the database
-func (repository *buildingRepository) ExistsByBuildingName(buildingName string) bool {
+func (r *buildingRepository) ExistsByBuildingName(buildingName string) bool {
 	var count int64
-	repository.db.Model(&model.Building{}).Where("name = ?", buildingName).Count(&count)
+	r.db.Model(&model.Building{}).Where("name = ?", buildingName).Count(&count)
 	return count > 0
 }
 
-// Find by id returns a building by its id
-func (repository *buildingRepository) FindByID(id int) (*model.Building, error) {
+func (r *buildingRepository) FindByID(id uint) (*model.Building, error) {
 	var building model.Building
-	err := repository.db.First(&building, id).Error
+	err := r.db.First(&building, id).Error
 	if err != nil {
 		return nil, err
 	}
 	return &building, nil
 }
 
-// FindAll returns all buildings
-func (repository *buildingRepository) FindAll() ([]model.Building, error) {
+func (r *buildingRepository) FindAll() ([]model.Building, error) {
 	var buildings []model.Building
-	err := repository.db.Find(&buildings).Error
+	err := r.db.Find(&buildings).Error
 	if err != nil {
 		return nil, err
 	}
 	return buildings, nil
 }
 
-// NewUserRepository creates a new UserRepository
 func NewBuildingRepository(db *gorm.DB) BuildingRepository {
-
-	repository := &buildingRepository{
-		db: db,
-	}
-
-	return repository
+	return &buildingRepository{db: db}
 }
