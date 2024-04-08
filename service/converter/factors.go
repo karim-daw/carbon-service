307e00f1f20e5583bfb629b42ff8b262bc3edb09
karim-daw <karimdaw91@gmail.com>
2024-04-08 09:19:37 +0100

    ADD: conversion factrors

---
+++ service/converter/factors.go
@@ -0,0 +1,33 @@
+package converter
+
+const (
+	// area conversion factors
+	M2_TO_FT2 = 10.7639
+	FT2_TO_M2 = 0.092903
+
+	// volume conversion factors
+	M3_TO_FT3 = 35.3147
+	FT3_TO_M3 = 0.0283168
+
+	// energy conversion factors
+	KWH_TO_KBTU = 3.41214
+	KBTU_TO_KWH = 0.293071
+
+	// mass conversion factors
+	KG_TO_LB = 2.20462
+	LB_TO_KG = 0.453592
+
+	// density conversion factors
+	KG_M3_TO_LB_FT3 = 0.0624279
+	LB_FT3_TO_KG_M3 = 16.0185
+
+	// carbon conversion factors
+	KGC02_TO_LBCO2 = 2.20462
+	LBCO2_TO_KGC02 = 0.453592
+
+	// KGC02/M2_TO_LBCO2/FT2
+	KGC02_PER_M2_TO_LBCO2_PER_FT2 = 0.204167
+	LBCO2_PER_FT2_TO_KGC02_PER_M2 = 4.88243
+
+	// Add more conversion factors as needed
+)
